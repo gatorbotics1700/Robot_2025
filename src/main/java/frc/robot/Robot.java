f4572d1986bf58f00d76adc0ff83563bb9e3fe47
Gatorbotics <gatorbotics1700@gmail.com>
2025-01-22 18:48:09 -0800

    added simple cameraserver program from wpilib docs

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -8,6 +9,10 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
     private RobotContainer container;
+    
+    public Robot() {
+        CameraServer.startAutomaticCapture();
+    }
 
     @Override
     public void robotInit() {
