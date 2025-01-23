23ae18ddd0cf349fc743086407a79e7bc65abc54
gatorbotics <gatorbotics1700@gmail.com>
2025-01-22 18:36:20 -0800

    added a bandaid fix for drivers practice

--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -108,7 +108,7 @@ public class DrivetrainSubsystem extends SubsystemBase {
                 kinematics,
                 new Rotation2d(Math.toRadians(pigeon.getYaw().getValueAsDouble())),
                 new SwerveModulePosition[]{ frontLeftModule.getPosition(), frontRightModule.getPosition(), backLeftModule.getPosition(), backRightModule.getPosition() },
-                new Pose2d(0, 0, new Rotation2d(Math.toRadians(0)))
+                new Pose2d(0, 0, new Rotation2d(Math.toRadians(90))) //bandaid fix
         );
         
         states=kinematics.toSwerveModuleStates(chassisSpeeds);
