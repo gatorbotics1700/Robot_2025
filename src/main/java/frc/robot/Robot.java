705b72dc3646399ed6c13f64fa2ae216e1bf00e5
Anaika Walia <anaikawalia@Anaikas-Laptop.local>
2025-01-25 12:50:23 -0800

    A

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,84 +1,20 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.BlinkinLEDController;
-import edu.wpi.first.wpilibj2.command.Command;
 
 public class Robot extends TimedRobot {
-    private Command m_teleopColor;
-    private Command m_teleopCommand1;
-    private Command m_autoColor;
-
-    private RobotContainer m_robotContainer;
     private BlinkinLEDController ledController;
-
 
     @Override
     public void robotInit() {
-        // Initialize RobotContainer
-        m_robotContainer = new RobotContainer();
+        // Initialize the LED controller on PWM port 0
         ledController = new BlinkinLEDController(0);
-
-    }
-
-    @Override
-    public void robotPeriodic() {
-      CommandScheduler.getInstance().run();
-      
     }
 
     @Override
     public void teleopInit() {
+        System.out.println("Setting LEDs to LIME...");
         ledController.setPattern(BlinkinLEDController.BlinkinPattern.LIME);
-
-        // m_teleopColor = m_robotContainer.getTeleopColor();
-
-        // if (m_teleopColor != null) {
-        //     m_teleopColor.schedule();
-        //     System.out.println("Scheduled teleopColor command to set LEDs to green.");
-
-        // }
-
-
     }
-
-    
-
-    @Override
-    public void teleopPeriodic() {
-    
-        // if (m_teleopColor != null) {
-        //     m_teleopColor.schedule();
-        //     System.out.println("Scheduled teleopColor command to set LEDs to green.");
-
-        // }
-    }
-
-
-    @Override
-    public void autonomousInit(){
-      
-    } 
-       
-    
-
-    @Override
-    public void autonomousPeriodic() {}
-
-    @Override
-    public void disabledInit() {}
-  
-    @Override
-    public void disabledPeriodic() {}
-
-    @Override
-    public void testInit() {
-      // Cancels all running commands at the start of test mode.
-     // CommandScheduler.getInstance().cancelAll();
-    }
-
-    @Override
-    public void testPeriodic() {}
 }
--- src/main/java/frc/robot/subsystems/BlinkinLEDController.java
+++ src/main/java/frc/robot/subsystems/BlinkinLEDController.java
@@ -2,33 +2,29 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
-import frc.robot.Constants;
-import edu.wpi.first.hal.can.CANJNI;
-import edu.wpi.first.hal.can.CANStatus;
 
-public class BlinkinLEDController extends SubsystemBase{
+public class BlinkinLEDController extends SubsystemBase {
     public enum BlinkinPattern {
-        //RAINBOW_PARTY_PALETTE(-0.97),
-        RED_ORANGE(+0.62), //good
-        LIME(+0.73), //good
-        PURPLE(+0.92); //test
+        RED_ORANGE(+0.62),
+        LIME(+0.73),
+        PURPLE(+0.92);
+
         public final double value;
-        private BlinkinPattern(double value) {
-          this.value = value;
+
+        BlinkinPattern(double value) {
+            this.value = value;
         }
-  };
+    }
 
- // private static BlinkinLEDController m_controller = null;
-  private Spark m_blinkin;
-  //private BlinkinPattern m_currentPattern;
+    private final Spark m_blinkin;
 
-  public BlinkinLEDController(int port) {
-    m_blinkin = new Spark(port);
-}
+    public BlinkinLEDController(int port) {
+        System.out.println("Initializing BlinkinLEDController on port: " + port);
+        m_blinkin = new Spark(port);
+    }
 
-public void setPattern(BlinkinPattern pattern) {
-    System.out.println("Setting LED pattern to: " + pattern.value);
-    m_blinkin.set(pattern.value);
-}
-
+    public void setPattern(BlinkinPattern pattern) {
+        System.out.println("Setting LED pattern to: " + pattern.name() + " (" + pattern.value + ")");
+        m_blinkin.set(pattern.value);
+    }
 }
