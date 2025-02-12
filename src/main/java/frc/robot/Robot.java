d2bb6bc75e3c34f2f3bff0be05ec01bf0a598603
Gatorbotics <gatorbotics1700@gmail.com>
2025-02-11 18:22:04 -0800

    attempted code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,10 +1,16 @@
 package frc.robot;
 
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.CvSink;
+import edu.wpi.first.cscore.CvSource;
+import edu.wpi.first.cscore.UsbCamera;
+import edu.wpi.first.util.PixelFormat;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.cscore.MjpegServer;
+
 
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
@@ -12,6 +18,21 @@ public class Robot extends TimedRobot {
     
     public Robot() {
         CameraServer.startAutomaticCapture();
+        CvSink cvSink = CameraServer.getVideo();
+        UsbCamera usbCamera = new UsbCamera("USB Camera 0",0);
+        MjpegServer mjpegServer1 = new MjpegServer("serve_USB Camera 0", 1181);
+        mjpegServer1.setSource(usbCamera);
+
+        CvSink cvsink = new CvSink("opencv_USB Camera 0");
+        cvSink.setSource(usbCamera);
+
+        CvSource outputStream = new CvSource ("Blur", PixelFormat.kMJPEG, 640, 480, 30);
+        MjpegServer mjpegServer2 = new MjpegServer("serve_Blur", 1182);
+        mjpegServer2.setSource(outputStream);
+
+
+
+
     }
 
     @Override
@@ -33,7 +54,7 @@ public class Robot extends TimedRobot {
         //     m_autonomousCommand.schedule();
         // }
 
-    }
+    }   
 
     @Override
     public void teleopInit() {
