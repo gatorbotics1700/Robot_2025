ce3fca37c43b2355233a57a8fff8820cc903638a
gatorbotics <gatorbotics1700@gmail.com>
2025-04-17 13:48:07 -0700

    champs changes - vision offsets and motor voltage for l4 shooting

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -63,12 +63,12 @@ public class Constants {
     public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(52.1191);
     public static final MechanicalConfiguration MODULE_CONFIGURATION = SdsModuleConfigurations.MK4I_L2;
     public static final String CANIVORE_BUS_NAME = "TRex";
-    public static final Pose3d LIMELIGHT_OFFSETS = new Pose3d(0.3394, 0.039, 0.196, new Rotation3d(Math.toRadians(1), Math.toRadians(-19.5), Math.toRadians(-3.0)));
+    public static final Pose3d LIMELIGHT_OFFSETS = new Pose3d(0.3394, 0.039 -0.03, 0.196, new Rotation3d(Math.toRadians(1), Math.toRadians(-19.5), Math.toRadians(-3.0)));
    public static final double CENTER_TO_BUMPER_OFFSET = 0.45164;
     public static final Pose2d FRONT_CENTER_ALIGN_OFFSET = new Pose2d(CENTER_TO_BUMPER_OFFSET, 0, new Rotation2d(0)); //offset from center of robot to where we want to line up with the april tag
     public static final Pose2d INTAKE_ALIGN_OFFSET = new Pose2d(CENTER_TO_BUMPER_OFFSET - 0.3, 0, new Rotation2d(0)); //offset from center of robot to where we want to line up with the april tag
-    public static final Pose2d SHOOTING_L4_LEFT_OFFSET = new Pose2d(CENTER_TO_BUMPER_OFFSET, -CENTER_TO_POST /*-0.161925*/, new Rotation2d(0)); //offset from center of robot to where we want to line up with the april tag
-    public static final Pose2d SHOOTING_L4_RIGHT_OFFSET = new Pose2d(CENTER_TO_BUMPER_OFFSET, CENTER_TO_POST /*0.161925*/, new Rotation2d(0)); //offset from center of robot to where we want to line up with the april tag
+    public static final Pose2d SHOOTING_L4_LEFT_OFFSET = new Pose2d(CENTER_TO_BUMPER_OFFSET +0.06, -CENTER_TO_POST /*-0.161925*/, new Rotation2d(0)); //offset from center of robot to where we want to line up with the april tag
+    public static final Pose2d SHOOTING_L4_RIGHT_OFFSET = new Pose2d(CENTER_TO_BUMPER_OFFSET+0.06, CENTER_TO_POST /*0.161925*/, new Rotation2d(0)); //offset from center of robot to where we want to line up with the april tag
     // //the line up offsets are the point (in robot relative coordinates) that needs to align with the apriltag in order for us to score left/right post 
     // //(we flip the values in our offset method so that we can find the pose the center of the robot needs to be at, but they should not be flipped here!)
 
@@ -106,7 +106,7 @@ public class Constants {
     public static final double CORAL_SHOOTING_MIN_CURRENT = 2.5;
     public static final double CORAL_INTAKING_VOLTAGE = 4.5;//5.0;//5.4; // switched from 4.0
     public static final double CORAL_PREINTAKE_SHOOTING_VOLTAGE = -1.0; 
-    public static final double CORAL_L4_SHOOTING_VOLTAGE = -11.0;//-10.6;//-8.0;//-8.1;//-7.8;
+    public static final double CORAL_L4_SHOOTING_VOLTAGE = -11.2; //-10.2;//-11;//-8.0;//-8.1;//-7.8;
     public static final double CORAL_TROUGH_SHOOTING_VOLTAGE = -5.5; // switched from -4.0
     public static final double CORAL_VOMIT_VOLTAGE = -0.3;
 
